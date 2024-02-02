fd4e9a4f01ec3d4287e94577c813a8f7289c5989
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2024-02-02 11:09:45 +0530

    lineChart added in BuyPage component

---
+++ src/Pages/BuyPage/LineChart1.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+
+function generateTimeIntervals(currentTime) {
+  const endDate = new Date(`2024-02-01 ${currentTime}`);
+  return endDate.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+}
+
+const LineChart1 = ({ socket }) => {
+  const [label, setLabel] = useState([]);
+  const [lineLabel, setLineLabel] = useState([]);
+
+
+  console.log("lable is ", label);
+  console.log("lineLable is ", lineLabel);
+
+
+
+
+
+  useEffect(() => {
+
+    const generateLable = (data) => {
+      // data -> avg stock / user
+      const curr = new Date(Date.now());
+      const currTime = `${curr.getHours()}:${curr.getMinutes()}`;
+      let interval = generateTimeIntervals(currTime);
+  
+
+      setLabel((x) => {
+        if (interval == x[x.length - 1]) {
+          setLineLabel((prevLine) => {
+            let lineLableCopy = [...prevLine];
+            lineLableCopy[lineLableCopy.length - 1] = data;
+            return lineLableCopy;
+          })
+          return [...x];
+        }
+        else {
+          setLineLabel((prev) => {
+            return [...prev, data];
+          })
+          return [...x, interval]
+        }
+      });
+      
+    };
+    socket.on("line-chart-data", generateLable);
+    return () => {
+      // socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <div>
+      <Line
+        datasetIdKey="id"
+        data={{
+          labels: label,
+          datasets: [
+            {
+              id: 1,
+              label: "",
+              data: lineLabel,
+            },
+          ],
+        }}
+      />
+    </div>
+  );
+};
+
+export default LineChart1;
