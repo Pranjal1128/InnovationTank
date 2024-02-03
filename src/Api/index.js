bf24c83dac8c2bc3b149a3830e9ab36b182b7700
lakshmanmulchandani <mulchandanilakshman@gmail.com>
2024-02-03 20:27:41 +0530

    audience ranking data fetched but need to add in table

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import backend_url from  "../config"
+import {backend_url} from  "../config"
 
-const url = `https://stocksup.onrender.com/portfolios`;
+const url = `${backend_url}/portfolios`;
 
 export const fetchPortfolios = () => axios.get(url);
 export const createPortfolio = (newPortfolio) => axios.post(url, newPortfolio);
